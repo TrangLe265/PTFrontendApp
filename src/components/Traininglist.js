f575ebd7a01452edcc6e5fed37ed5b1a1d378b30
TrangLe265 <trang.le@myy.haaga-helia.fi>
2024-11-15 13:00:23 +0200

    Added Search functions and refactored codes

--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -8,15 +8,28 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import '../styling/index.css';
 
-import { format, isValid, parseISO } from 'date-fns';
+import { Snackbar, Typography } from '@mui/material';
+import { TextField } from '@mui/material';
+import Box from '@mui/material/Box'; 
+import Grid from '@mui/material/Grid2';
+
+import {fetchTrainings,deleteTraining} from '../functions/fetching';
 
 export default function Traininglist(){
     
     const [trainings, setTrainings] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [open, setOpen] = useState(false);
 
     const [columnsDefs, setColumnDefs] = useState([
         {headerName:'Action', flex:1,
-            cellRenderer: params => DeleteButton(params.data._links.self.href)
+            cellRenderer: params =>
+                (<IconButton 
+                    aria-label='delete' 
+                    color='white' 
+                    onClick={() => handleDeleteTraining(params.data._links.self.href)}> 
+                    <DeleteIcon /> 
+                </IconButton>)
         },
         {field: 'activity', flex:2},
         {field: 'date',  flex:2},
@@ -24,58 +37,69 @@ export default function Traininglist(){
         {field: 'customer', flex:1},
     ])
 
-    const DeleteButton = (url) => {
-        return <IconButton aria-label='delete' color='white' onClick={() => deleteTraining(url)}> <DeleteIcon /> </IconButton>
-    }
-    
-    const deleteTraining = (url) => {
-        fetch(url, {method: 'DELETE'})
-        .then(res => fetchTrainings())
-        .catch(err => console.error(err))
-        console.log(url); 
+
+    useEffect(() => {handleFetchTrainings()}, []); 
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
     }
 
-
-    useEffect(() => {fetchTrainings()}, []); 
-
-    const fetchTrainings = async () => {
+    const handleFetchTrainings = async () => {
         try {
-            const response = await fetch('https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/trainings'); 
-            const data = await response.json(); 
-            const trainings = data._embedded.trainings; 
+            const data = await fetchTrainings();
+            console.log(data);
+            setTrainings(data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
-            //mapped all fetched trainings above to add the extra customer field 
-            const trainedCustomer = await Promise.all(trainings.map(
-                async (training) => {
-                    const customerDataResponse = await fetch(training._links.customer.href); 
-                    const trainingDate = parseISO(training.date)
-                    const customerData = await customerDataResponse.json(); 
-                    return {
-                        ...training, //keeping all the previous data using spread operator
-                        customer: `${customerData.firstname} ${customerData.lastname}`,
-                        date: isValid(trainingDate) ? format(new Date(trainingDate), 'dd.MM.yyyy hh:mm a' ) : 'Invalid Date'
-                    }
-                })); 
-                setTrainings(trainedCustomer); 
-        } catch (error) {
-            console.log('Error in fetching: ', error);
-        } 
-    }; 
+    const handleDeleteTraining = (url) => {
+        if (window.confirm('Are you sure you want to delete this training?')) {
+            deleteTraining(url)
+            .then(() => {
+                handleFetchTrainings();
+                setOpen(true);
+            })
+            .catch((err) => console.error(err));
+        }
+    }
 
-    console.log(trainings); 
+    const filteredTrainings = trainings.filter(
+        (training) => training.activity.toLowerCase().includes(searchQuery.toLowerCase()) || training.customer.toLowerCase().includes(searchQuery.toLowerCase()));
 
     return(
-        <>
+        <> 
+            <Box sx={{ flexGrow: 1 , marginBottom: '10px'}}>
+            <Grid container spacing={2}>
+                <Grid size={4}>
+                    <Typography variant="h3" sx={{color:'#0B132B'}}>Trainings List</Typography>  
+                </Grid>
+                <Grid size={8}>
+                    <TextField fullWidth label="Search" variant="outlined"
+                                value={searchQuery}
+                                onChange={handleSearchChange}
+                                style={{marginBottom:'10px'}}
+                    />
+                </Grid>      
+            </Grid>
+            </Box>
+  
             <div className='ag-theme-quartz-dark' style={{height:'500px'}} >
                 <AgGridReact
-                    rowData = {trainings}
+                    rowData = {filteredTrainings}
                     columnDefs={columnsDefs}
                     pagination={true}
                     paginationAutoPageSize={true}
                     suppressCellFocus={true }
                 />
-                
             </div>
+            <Snackbar 
+                open = {open}
+                autoHideDuration={3000}
+                onClose = {() => setOpen(false)}
+                message = 'Training deleted successfully'
+            />
         </>
     )
 }
---
+++ src/functions/fetching.js
@@ -0,0 +1,34 @@
+import { parseISO, isValid, format } from 'date-fns';
+import { useState } from 'react';
+
+export const fetchTrainings = async() => {
+    try {
+        const response = await fetch('https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/trainings'); 
+        const data = await response.json(); 
+        const trainings = data._embedded.trainings; 
+
+        // Mapped all fetched trainings above to add the extra customer field 
+        const trainedCustomer = await Promise.all(trainings.map(
+            async (training) => {
+                const customerDataResponse = await fetch(training._links.customer.href); 
+                const trainingDate = parseISO(training.date);
+                const customerData = await customerDataResponse.json(); 
+                return {
+                    ...training, // Keeping all the previous data using spread operator
+                    customer: `${customerData.firstname} ${customerData.lastname}`,
+                    date: isValid(trainingDate) ? format(new Date(trainingDate), 'dd.MM.yyyy hh:mm a' ) : 'Invalid Date'
+                };
+            }
+        )); 
+        
+        return trainedCustomer;
+    } catch (error) {
+        console.log('Error in fetching: ', error);
+        return [];
+    } 
+}
+
+export const deleteTraining = (url) => {
+    return fetch(url, {method: 'DELETE'})
+    .catch(err => console.error(err))
+}
